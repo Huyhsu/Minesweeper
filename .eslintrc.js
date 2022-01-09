9e0d533456713e476381d94d35aa0b3897b5f90a
Huyhsu <79528605+Huyhsu@users.noreply.github.com>
2022-01-09 13:43:04 +0800

    Add webpack and babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
+    'no-var-requires': false,
   },
 };
--- src/test.js
+++
@@ -1,9 +0,0 @@
-var a;
-var b;
-var c;
-var user = {
-  name: 'Huy',
-  surname: 'hsu',
-  age: 20,
-  isAdmiin: true,
-};
